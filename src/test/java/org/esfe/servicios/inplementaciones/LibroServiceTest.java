8c4ffbf0de9811a2998b90901778e4b0e8c1e199
avsh1 <sh23002@esfe.agape.edu.sv>
2024-12-02 20:30:20 -0600

    hola

---
+++ src/test/java/org/esfe/servicios/inplementaciones/LibroServiceTest.java
@@ -0,0 +1,32 @@
+package org.esfe.servicios.inplementaciones;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibroServiceTest {
+
+    @Test
+    void obtenerTodos() {
+    }
+
+    @Test
+    void obtenerTodosPaginados() {
+    }
+
+    @Test
+    void obtenerPorId() {
+    }
+
+    @Test
+    void crear() {
+    }
+
+    @Test
+    void editar() {
+    }
+
+    @Test
+    void eliminarPorId() {
+    }
+}
